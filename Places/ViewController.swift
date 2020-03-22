421778d23e7f4e4b0aac0940b3eef66433813569
Sebastian Cancinos <sebacancinos@gmail.com>
2020-03-22 10:08:41 +0100

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Sebastian Cancinos on 22/03/2020.
+//  Copyright © 2020 sebacancinos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
